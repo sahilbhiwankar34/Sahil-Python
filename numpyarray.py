a05bc3bc0b1c882ce62765ef9df518a7c5b06677
Sahil Bhiwankar <sahilbhiwankar34@gmail.com>
2026-02-03 19:43:24 +0530

    Add files via upload

---
+++ numpyarray.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Creating the array
+arr = np.array([[1, 2, 3],
+                [4, 5, 6]])
+
+print("Array:")
+print(arr)
+
+# a) Number of dimensions
+print("\nNumber of dimensions:", arr.ndim)
+
+# b) Shape of the array
+print("Shape of the array:", arr.shape)
+
+# c) Size of the array
+print("Size of the array:", arr.size)
+
+# d) Type of elements
+print("Type of elements:", arr.dtype)
